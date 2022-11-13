fbc9b36ce342de5295d8ed8c775615cfaea15193
Aarav Bajaj <aaravbajaj012@gmail.com>
2022-11-13 17:25:48 -0500

    remove unnecessary imports

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,5 @@
 import type { NextPage } from 'next'
-import Image from 'next/image'
 import { AdminLayout } from '@layout'
-import {
-  Button, ButtonGroup, Card, Dropdown, ProgressBar,
-} from 'react-bootstrap'
 import React from 'react'
 
 const Home: NextPage = () => (
