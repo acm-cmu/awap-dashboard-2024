ab5e08f9190332728ab4588f78025040ce70e3ff
Meng87 <79801878+Meng87@users.noreply.github.com>
2024-01-16 02:46:39 -0500

    switch admin match history to single table format

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -4,11 +4,76 @@ import { useSession } from 'next-auth/react';
 import Router from 'next/router';
 import { useEffect } from 'react';
 import axios, { AxiosError, AxiosResponse } from 'axios';
-import { Button, Card } from 'react-bootstrap';
+import { Button, Card, Table } from 'react-bootstrap';
 import { toast } from 'react-toastify';
+import useSWR from 'swr';
+import { Match } from '@pages/api/admin/admin-match-history';
+
+const TableRow: React.FC<{ match: Match }> = ({ match }) => {
+  let outcomeMsg = '';
+  if (match.outcome === 'team1') {
+    outcomeMsg = match.player1;
+  } else if (match.outcome === 'team2') {
+    outcomeMsg = match.player2;
+  } else {
+    outcomeMsg = 'unknown';
+  }
+
+  return (
+    <tr>
+      <td>{match.id}</td>
+      <td>{match.player1}</td>
+      <td>{match.player2}</td>
+      <td>{match.category}</td>
+      <td>{match.status}</td>
+      <td>{outcomeMsg}</td>
+      <td>
+        <a href={match.replay}>Download</a>
+      </td>
+    </tr>
+  );
+};
+
+const TableBody: React.FC<{ data: Match[] }> = ({ data }) => {
+  if (data == null) {
+    console.log('data is null');
+  } else if (data.length == 0) {
+    console.log('data is zero');
+  }
+  // eslint-disable-next-line react/jsx-key
+  return (
+    <tbody>
+      {data && data.map((item: Match) => <TableRow match={item} />)}
+    </tbody>
+  );
+};
+
+const MatchTable: React.FC<{ data: Match[] }> = ({ data }) => (
+  <Table striped bordered hover className='text-center'>
+    <thead>
+      <tr>
+        <th>Match ID</th>
+        <th>Player 1</th>
+        <th>Player 2</th>
+        <th>Category</th>
+        <th>Status</th>
+        <th>Outcome</th>
+        <th>Replay</th>
+      </tr>
+    </thead>
+    <TableBody data={data} />
+  </Table>
+);
 
 const Admin: NextPage = () => {
   const { status, data } = useSession();
+
+  const fetcher = async (url: string) => axios.get(url).then((res) => res.data);
+
+  const { data: MatchData, mutate } = useSWR(
+    '/api/admin/admin-match-history',
+    fetcher
+  );
 
   useEffect(() => {
     if (status === 'unauthenticated') Router.replace('/auth/login');
@@ -133,6 +198,22 @@ const Admin: NextPage = () => {
                   Start Ranked Scrimmages
                 </Button>
               </Card.Text>
+            </Card.Body>
+          </Card>
+          <br />
+          <Card>
+            <Card.Body>
+              <Card.Title>Global Match History</Card.Title>
+              <Button
+                variant='primary'
+                className='mb-3'
+                onClick={async () => {
+                  mutate();
+                }}
+              >
+                Refresh
+              </Button>
+              <MatchTable data={MatchData} />
             </Card.Body>
           </Card>
         </UserLayout>
